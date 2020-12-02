9bee2344aef704335fb97df27083b36bd706d45b
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-02 08:21:35 +0000

    詳細ページを作成しました

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end  
 
   def edit
