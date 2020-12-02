daccb4014dda439a54293757ae48ce021f592221
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-02 08:41:32 +0000

    編集機能を実装しました

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,7 +18,15 @@ class BlogsController < ApplicationController
   end  
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
   
   private
   def blog_params
